d922b1632d00d223279a74578e5f0fdb4c3894f8
Scanlon Rebelo <scanlon@edifytech.us>
2023-07-13 21:17:08 +0530

    class based component, contructor for state mgmt, component life cycle hooks and error boundaries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
 
-import Users from './components/Users';
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div>
-      <Users />
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
